ca32418ef1af6df80e21fffb2ccf8ed239bf7201
Javis <jelvismbah18@gmail.com>
2025-01-31 10:51:41 +0100

    change ship, alien and game interface design

--- settings.py
+++ settings.py
@@ -6,16 +6,16 @@ class Settings:
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (0, 0, 0)
 
         # Ship settings
         self.ship_limit = 4
 
         # Bullet settings
-        self.bullet_width = 3
-        self.bullet_height = 15
-        self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 4
+        self.bullet_width = 4
+        self.bullet_height = 20
+        self.bullet_color = (250, 157, 17)
+        self.bullets_allowed = 5
 
         # Alien settings
         self.fleet_drop_speed = 10
