2d156d875886fbb2278d6ac36c375544d19b1d09
Javis <jelvismbah18@gmail.com>
2025-01-31 10:09:26 +0100

    update game settings

--- settings.py
+++ settings.py
@@ -9,13 +9,13 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # Ship settings
-        self.ship_limit = 3
+        self.ship_limit = 4
 
         # Bullet settings
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 3
+        self.bullets_allowed = 4
 
         # Alien settings
         self.fleet_drop_speed = 10
@@ -30,8 +30,8 @@ class Settings:
     def initialize_dynamic_settings(self):
         """Initialize settings that change throughout the game."""
         self.ship_speed = 1.5
-        self.bullet_speed = 3.0
-        self.alien_speed = 1.0
+        self.bullet_speed = 3.5
+        self.alien_speed = 0.5
 
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
