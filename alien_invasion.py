ca1dc0a5cd0599c8281ce63ea2de1a49a579ff19
RedwolfDark <30651692+RedwolfDark@users.noreply.github.com>
2025-03-07 15:14:38 +0100

    add bug again

--- alien_invasion.py
+++ alien_invasion.py
@@ -247,7 +247,3 @@ class AlienInvasion:
             self.play_button.draw_button()
 
         pygame.display.flip()
-
-if __name__ == '__main__':
-    # Make a game instance, and run the game.
-    ai.run_game()
